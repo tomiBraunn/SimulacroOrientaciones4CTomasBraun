1a69df9a734ff9ca88767e021d2660d750700b6f
Tomas B <141858115+tomiBraunn@users.noreply.github.com>
2025-04-15 08:50:38 -0300

    por las dudas

---
+++ SimulacroExamen/Assets/deQueOrientacionSos.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deQueOrientacionSos : MonoBehaviour
+{
+    public string nombreAlumno;
+    public int añoAlumno;
+    public string especialidadAlumno;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (nombreAlumno == "")
+        {
+            Debug.Log("Porfavor ingrese un nombre.");
+            return;
+        }
+
+        if (añoAlumno < 1 || añoAlumno > 5)
+        {
+            Debug.Log("Porfavor ingrese un año valido.");
+            return;
+        }
+
+        if (añoAlumno < 3 || añoAlumno > 5)
+        {
+            Debug.Log("Error. Aún estás en el ciclo básico.");
+            return;
+        }
+
+        if (especialidadAlumno == "T" || especialidadAlumno == "D" || especialidadAlumno == "G" || especialidadAlumno == "M" || especialidadAlumno == "H")
+        {
+            Debug.Log("Muchas gracias "+ nombreAlumno + "!");
+            return;
+        }
+
+        else
+        {
+            Debug.Log("Solo puede ingresar T, D, G, M o H como tu especialidad.");
+            return;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
